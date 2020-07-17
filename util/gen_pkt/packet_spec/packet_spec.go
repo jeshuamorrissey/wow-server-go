184339b8bcf2658673392254bc65ac7794436448
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2020-07-17 13:46:52 +1000

    .

--- util/gen_pkt/packet_spec/packet_spec.go
+++ util/gen_pkt/packet_spec/packet_spec.go
@@ -1,15 +1,64 @@
 package packetspec
 
+// PacketSpec represents the basic structure of a packet spec file.
+// It is made up of one or more enum or packet.
 type PacketSpec struct {
-	Packet []*Packet `@@*`
+	Packets []*Packet `@@*`
 }
 
+type Enum struct {
+	Name   string       `"enum" @Ident`
+	Values []*EnumValue `"{" ( @@ )* "}"`
+}
+
+type EnumValue struct {
+	Key   string `@Ident`
+	Value string `"=" @Ident`
+}
+
+// Packet defines the syntax for a packet specification. Packets are
+// the word "packet" followed by a name followed by the definition in
+// {} brackets.
 type Packet struct {
-	Name   string   `"packet" @Ident`
-	Fields []*Field `"{" { @@ } "}"`
+	Name    string         `"packet" @Ident`
+	Entries []*PacketEntry `"{" ( @@ )* "}"`
+}
+
+// PacketEntry represents a single logical thing within a packet.
+// This could be a basic field, and inline struct definition or a
+// conditional.
+type PacketEntry struct {
+	IfConditional *IfConditional `"if" @@`
+	Struct        *Struct        `| "struct" @@`
+	Field         *Field         `| @@`
+}
+
+type IfConditional struct {
+	Var1      string         `"(" @Ident( @"." @Ident )?`
+	Operation string         `@Ident`
+	Var2      string         `@Ident( @"." @Ident )? ")"`
+	Entries   []*PacketEntry `"{" ( @@ )* "}"`
+}
+
+type Struct struct {
+	Name    string         `@Ident`
+	Entries []*PacketEntry `"{" ( @@ )* "}"`
 }
 
 type Field struct {
-	Type string `@Ident`
-	Name string `@Ident ";"`
+	Type     *Type    `@@`
+	Name     string   `@Ident`
+	ArrayLen int      `( "[" @Int "]" )?`
+	Default  *Default `( "=" @@ )?`
+}
+
+type Default struct {
+	String string  `@String`
+	Int    int     `| @Int`
+	Float  float64 `| @Float`
+}
+
+type Type struct {
+	Name string `@Ident`
+	Size int    `( "[" @Int "]" )?`
 }
--- util/gen_pkt/packet_spec/packet_spec_test.go
+++ util/gen_pkt/packet_spec/packet_spec_test.go
@@ -3,42 +3,88 @@ package packetspec
 import (
 	"testing"
 
+	"fmt"
 	packet_spec "util/gen_pkt/packet_spec"
 
 	"github.com/alecthomas/participle"
-	"github.com/stretchr/testify/assert"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func parsePacketSpec(t *testing.T, packetSpec string) *packet_spec.PacketSpec {
 	parser, err := participle.Build(&packet_spec.PacketSpec{})
 	if err != nil {
-		assert.NoError(t, err)
+		fmt.Printf("%v\n", err)
+		t.Fail()
+		// assert.NoError(t, err)
 		return nil
 	}
 
 	spec := &packet_spec.PacketSpec{}
 	err = parser.ParseString(packetSpec, spec)
 	if err != nil {
-		assert.NoError(t, err)
+		fmt.Printf("parse error: %v\n", err)
+		t.Fail()
+		// assert.NoError(t, err)
 		return nil
 	}
 
 	return spec
 }
 
-func TestSingleSimplePacket(t *testing.T) {
+// func TestSingleSimplePacket(t *testing.T) {
+// 	spec := parsePacketSpec(t, `
+// packet Test {
+// 	int foo;
+// 	int bar;
+// }
+// `)
+
+// 	assert.Equal(t, 1, len(spec.Packet))
+// 	assert.Equal(t, "Test", spec.Packet[0].Name)
+// 	assert.Equal(t, 2, len(spec.Packet[0].Fields))
+// 	assert.Equal(t, "int", spec.Packet[0].Fields[0].Type)
+// 	assert.Equal(t, "foo", spec.Packet[0].Fields[0].Name)
+// 	assert.Equal(t, "int", spec.Packet[0].Fields[1].Type)
+// 	assert.Equal(t, "bar", spec.Packet[0].Fields[1].Name)
+// }
+
+func TestSingleComplexPacket(t *testing.T) {
 	spec := parsePacketSpec(t, `
-packet Test {
-	int foo;
-	int bar;
-}	
+packet ClientLoginChallenge {
+	string[4] game_name
+	int8 version[3]
+	int16 build
+	string[4] platform
+	string[4] os
+	string[4] locale
+	int32 timezone_offset
+	int32b ip_address
+	string account_name
+}
+
+packet ServerLoginChallenge {
+    int8 unk
+	Error error
+
+	if (error is Error.OK) {
+		struct challenge {
+			bigint[32] B
+			int8 g_len
+			int8 g
+			int8 N_len
+			bigint[32] N
+			bigint[32] salt
+			bigint[16] crc_salt
+			int8 unk = 0
+		}
+	}
+}
 `)
 
-	assert.Equal(t, 1, len(spec.Packet))
-	assert.Equal(t, "Test", spec.Packet[0].Name)
-	assert.Equal(t, 2, len(spec.Packet[0].Fields))
-	assert.Equal(t, "int", spec.Packet[0].Fields[0].Type)
-	assert.Equal(t, "foo", spec.Packet[0].Fields[0].Name)
-	assert.Equal(t, "int", spec.Packet[0].Fields[1].Type)
-	assert.Equal(t, "bar", spec.Packet[0].Fields[1].Name)
+	// assert.Equal(t, 1, len(spec.Packets))
+	// assert.Equal(t, "UpdateObject", spec.Packets[0].Name)
+
+	spew.Dump(spec)
+	t.Fail()
+
 }
